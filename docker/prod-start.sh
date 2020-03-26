0ba21e6b216bfb49c16f49478d7c57ff318923db
Dmitriy <dmitriy4biz@gmail.com>
2020-03-26 07:41:52 -0300

    'local' and 'prod' Spring profiles were created

--- docker/prod-start.sh
+++ docker/prod-start.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR=$(readlink -f $(dirname "$0"))
 ROOT_DIR=$(dirname ${SCRIPT_DIR})
 
 docker build -t spr -f ${SCRIPT_DIR}/Dockerfile ${ROOT_DIR}
-docker run -it -d --rm --name SpringLearn --publish=8080:8080 spr
+docker run -it -d --rm --name SpringLearn --env  SPRING_PROFILES_ACTIVE=prod --publish=8080:8080 spr
--- src/main/java/com/skovdev/springlearn/config/OpenTableDBConfig.java
+++ src/main/java/com/skovdev/springlearn/config/OpenTableDBConfig.java
@@ -3,12 +3,14 @@ package com.skovdev.springlearn.config;
 import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.SQLException;
 
 @Configuration
+@Profile("local")
 public class OpenTableDBConfig {
 
     @Bean
