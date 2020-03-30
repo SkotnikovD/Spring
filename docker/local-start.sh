f59803d11a83921ca306a8b4dc04034f72ec1d73
Dmitriy <dmitriy4biz@gmail.com>
2020-03-30 09:26:12 -0300

    Added dedicated network for Docker environment

--- docker/local-start.sh
+++ docker/local-start.sh
@@ -3,4 +3,5 @@ SCRIPT_DIR=$(readlink -f $(dirname "$0"))
 ROOT_DIR=$(dirname ${SCRIPT_DIR})
 
 docker build -t spr -f ${SCRIPT_DIR}/Dockerfile ${ROOT_DIR}
-docker run -it -d --rm --name SpringLearn SPRING_PROFILES_ACTIVE=local --publish=8080:8080 spr
+docker network create springlearn-net
+docker run -it -d --rm --name springlearn-be --env SPRING_PROFILES_ACTIVE=local --network springlearn-net --publish=8080:8080 spr
